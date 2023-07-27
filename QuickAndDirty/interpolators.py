82989b13bfd705fa63bf5c7ee8934d452cae1705
Thibault Monsel <thibaultmonsel@gmail.com>
2023-07-27 16:33:29 +0200

    interpol simplification + cleanup 2

--- QuickAndDirty/interpolators.py
+++ QuickAndDirty/interpolators.py
@@ -2,38 +2,39 @@ import numpy as np
 import torch
 
 
-class TorchLinearInterpolator():
+class TorchLinearInterpolator:
+    def __init__(self, ts, ys, device):
 
-    def __init__(self,ts, ys, device):
-
-        self.ts = ts.to(device) # [N_t]
-        self.ys = ys.to(device) # [N, N_t, D]
+        self.ts = ts.to(device)  # [N_t]
+        self.ys = ys.to(device)  # [N, N_t, D]
 
     def __post_init__(self):
         if self.ts.ndim != 1:
             raise ValueError("`ts` must be one dimensional.")
-        
-        if self.ys.shape[0] != self.ts.shape[0]:
-                raise ValueError(
-                    "Must have ts.shape[0] == ys.shape[0], that is to say the same "
-                    "number of entries along the timelike dimension."
-                )
-        
-        if np.all(np.diff(self.ts) > 0) :
+
+        if self.ys.shape[1] != self.ts.shape[0]:
+            raise ValueError(
+                "Must have ts.shape[0] == ys.shape[0], that is to say the same "
+                "number of entries along the timelike dimension."
+            )
+
+        if np.all(np.diff(self.ts) > 0):
             raise ValueError("`ts` must be monotonically increasing.")
-                
-    def _interpret_t(self, t : float, left: bool):
+
+    def _interpret_t(self, t: float, left: bool):
         maxlen = self.ts.shape[0] - 2
         index = torch.searchsorted(self.ts, t, side="left" if left else "right")
         index = torch.clip(index - 1, 0, maxlen)
         # Will never access the final element of `ts`; this is correct behaviour.
         fractional_part = t - self.ts[index]
         return index, fractional_part
-    
+
     def __call__(self, t, left=True):
-        
+
         if t > self.ts[-1] or t < self.ts[0]:
-            raise ValueError("Interpolation point is outside data range. ie t > ts[-1] or t < ts[0]")
+            raise ValueError(
+                "Interpolation point is outside data range. ie t > ts[-1] or t < ts[0]"
+            )
         index, fractional_part = self._interpret_t(t, left)
         prev_ys = self.ys[index]
         next_ys = self.ys[index + 1]
@@ -42,7 +43,25 @@ class TorchLinearInterpolator():
         diff_t = next_t - prev_t
 
         return prev_ys + (next_ys - prev_ys) * (fractional_part / diff_t)
-
-
-
-
+    
+    def add_point(self, new_t, new_y):
+        # new_t : float
+        # new_y : torch.tensor size [N, 1, D]
+        new_y = new_y.to(self.ts.device)
+        
+        if self.ys.shape[-1] != new_y.shape[-1]:
+            raise ValueError("You tried to add a new value that doesn't fit the shape of self.ys ")
+        
+        index, fractional_part = self._interpret_t(new_t, left=True)
+        if index == 0 :
+            new_ys, new_ts = torch.concat([new_y, self.ys], dim=1), torch.concat([new_t, self.ts], dim=0)
+        elif index == self.ts.shape[0] - 2:
+            new_ys, new_ts = torch.concat([self.ys, new_y], dim=1), torch.concat([self.ts, new_t], dim=0)
+        else : 
+            new_ys = torch.concat([self.ys[:index], new_y, self.ys[index:]], dim=1)
+            new_ts = torch.concat([self.ts[:index], new_t, self.ts[index:]], dim=0)
+        
+        self.ys = new_ys
+        self.ts = new_ts
+        if np.all(np.diff(self.ts) > 0):
+            raise ValueError("`ts` must be monotonically increasing. oups errors in add_point")
--- QuickAndDirty/nnde_adjoint.py
+++ QuickAndDirty/nnde_adjoint.py
@@ -1,9 +1,12 @@
-import torch
 import numpy as np
 import scipy
 import scipy.integrate as sciinteg
+import torch
 import torch.nn as nn
+
 from . import interpolators
+
+
 #
 # Implements a version of the NeuralODE adjoint optimisation algorithm, with the Adaptive Checkpoint Adjoint method
 #
@@ -11,9 +14,9 @@ from . import interpolators
 # ACA version : https://arxiv.org/abs/2006.02493
 #
 # The forward integration is done through the scipy IVP solver.
-# The backpropagation is based on the timesteps chosen by the scipy solver and only requires a step function for 
-# the relevant scheme to be added in the code. 
-# Any explicit scheme available with the scipy solver can be easily added here. 
+# The backpropagation is based on the timesteps chosen by the scipy solver and only requires a step function for
+# the relevant scheme to be added in the code.
+# Any explicit scheme available with the scipy solver can be easily added here.
 
 #
 # NB : This code is heavily based on the torch_ACA package (https://github.com/juntang-zhuang/torch_ACA)
@@ -22,27 +25,25 @@ from . import interpolators
 # Used for float comparisons
 tiny = 1e-8
 
-class nddeint_ACA(torch.autograd.Function):
 
+class nddeint_ACA(torch.autograd.Function):
     @staticmethod
     def forward(ctx, history, func, options, *params):
 
-        
-
         # Saving parameters for backward()
         ctx.func = func
-        #ctx.flat_params = flat_params
+        # ctx.flat_params = flat_params
         with torch.no_grad():
             ctx.save_for_backward(*params)
         ctx.options = options
-        ctx.nSteps = options['nSteps']
-        ctx.dt = options['dt']
-        t = options['t0']
+        ctx.nSteps = options["nSteps"]
+        ctx.dt = options["dt"]
+        t = options["t0"]
         val = history(t)
         delays = func.delays
         # Device used
         argdev = val.device
-        
+
         argshape = val.shape
         argdtype = val.dtype
 
@@ -51,18 +52,20 @@ class nddeint_ACA(torch.autograd.Function):
             values = [val]
             alltimes = [t]
             for i in range(ctx.nSteps):
-                val = val + ctx.dt * ctx.func(t,val,*[history(t-tau) for tau in delays])
+                val = val + ctx.dt * ctx.func(
+                    t, val, *[history(t - tau) for tau in delays]
+                )
                 t += ctx.dt
-                history.add_point(t,val)
+                history.add_point(t, val)
                 values.append(val)
                 alltimes.append(t)
         # Retrieving the time stamps selected by the solver
         ctx.alltimes = alltimes
         ctx.history = history
-        
+
         ctx.allstates = torch.stack(values)
-        evaluations = ctx.allstates[options['eval_idx']]
-        
+        evaluations = ctx.allstates[options["eval_idx"]]
+
         return evaluations
 
     @staticmethod
@@ -71,7 +74,7 @@ class nddeint_ACA(torch.autograd.Function):
 
         # grad_output holds the gradient of the loss w.r.t. each evaluation step
         grad_output = grad_y[0]
-        
+
         # h is the value of the forward time step
         h = ctx.dt
 
@@ -81,63 +84,61 @@ class nddeint_ACA(torch.autograd.Function):
 
         # f_params holds the NDDE parameters for which a gradient will be computed
         params = ctx.saved_tensors
-        
 
         # The last step of the time mesh is an evaluation step, thus the adjoint state which corresponds to the
         # gradient of the loss w.r.t. the evaluation states is initialised with the gradient corresponding to
         # the last evaluation time.
 
         adjoint_state = grad_output[-1]
-        i_ev=len(time_mesh)-2
+        i_ev = len(time_mesh) - 2
 
         adjoint_history = interpolators.TorchLinearInterpolator(
-                            adjoint_state.reshape(adjoint_state.shape[0],1,*adjoint_state.shape[1:]),
-                            torch.tensor(time_mesh[-1]).reshape(1).to(adjoint_state.device))
-        
+            adjoint_state.reshape(adjoint_state.shape[0], 1, *adjoint_state.shape[1:]),
+            torch.tensor(time_mesh[-1]).reshape(1).to(adjoint_state.device),
+        )
+
         out2 = None
 
         # The adjoint state as well as the parameters' gradient are integrated backwards in time.
         # Following the Adaptive Checkpoint Adjoint method, the time steps and corresponding states of the forward
         # integration are re-used by going backwards in the time mesh.
-        for i in range(len(time_mesh),0,-1):
-            t = time_mesh[i-1]
-            if i!=len(time_mesh):
+        for i in range(len(time_mesh), 0, -1):
+            t = time_mesh[i - 1]
+            if i != len(time_mesh):
                 adjoint_history.add_point(t, adjoint_state)
             # Backward Integrating the adjoint state and the parameters' gradient between time i and i-1
 
-            
             with torch.enable_grad():
                 # Taking a step with the NODE function to build a graph which will be differentiated
                 # so as to integrate the adjoint state and the parameters' gradient
-                derivative = 0.
+                derivative = 0.0
 
-                
-                
-                z_var = torch.autograd.Variable(ctx.history(t),requires_grad=True)
+                z_var = torch.autograd.Variable(ctx.history(t), requires_grad=True)
 
-                for tau in ctx.func.delays+[0]:
-                    if (t+tau - time_mesh[-1]) < tiny:
-                        temp_adjoint = adjoint_history(t+tau)
+                for tau in ctx.func.delays + [0]:
+                    if (t + tau - time_mesh[-1]) < tiny:
+                        temp_adjoint = adjoint_history(t + tau)
                         delayed_values = []
                         for tau2 in ctx.func.delays:
-                            if abs(tau-tau2) < tiny:
+                            if abs(tau - tau2) < tiny:
                                 delayed_values.append(z_var)
                             else:
-                                delayed_values.append(ctx.history(t+tau-tau2))
-                                
+                                delayed_values.append(ctx.history(t + tau - tau2))
+
                         if tau == 0:
-                            out = ctx.func(t,z_var,*delayed_values)
+                            out = ctx.func(t, z_var, *delayed_values)
                         else:
-                            out = ctx.func(t+tau,ctx.history(t+tau),*delayed_values)
-                        if tau==0:
-                            param_derivative_inc =  torch.autograd.grad(
-                                            out, params,
-                                            adjoint_state, retain_graph=True)
+                            out = ctx.func(
+                                t + tau, ctx.history(t + tau), *delayed_values
+                            )
+                        if tau == 0:
+                            param_derivative_inc = torch.autograd.grad(
+                                out, params, adjoint_state, retain_graph=True
+                            )
 
-                        derivative_inc = torch.autograd.grad(
-                                            out,  z_var,
-                                            -temp_adjoint)[0]
-
+                        derivative_inc = torch.autograd.grad(out, z_var, -temp_adjoint)[
+                            0
+                        ]
 
                         derivative = derivative + derivative_inc
 
@@ -150,33 +151,28 @@ class nddeint_ACA(torch.autograd.Function):
                 for _1, _2 in zip([*out2], [*param_derivative_inc]):
                     _1 = _1 - ctx.dt * _2
             # When reaching an evaluation step, the adjoint state is incremented with the gradient of the corresponding
-            # evaluation step 
-            next_i=i-1
-            if next_i in ctx.options['eval_idx'] and i!=len(time_mesh):
+            # evaluation step
+            next_i = i - 1
+            if next_i in ctx.options["eval_idx"] and i != len(time_mesh):
                 adjoint_state += grad_output[i_ev]
                 i_ev = i_ev - 1
 
-
-
-
         # Returning the gradient value for each forward() input
-        out = tuple([None] + [None,  None])+out2
-    
+        out = tuple([None] + [None, None]) + out2
 
         return out
 
 
-        
 def nddesolve_adjoint(history, func, options):
     # Main function to be called to integrate the NODE
 
     # z0 : (tensor) Initial state of the NODE
     # func : (torch Module) Derivative of the NODE
-    # options : (dict) Dictionary of solver options, should at least have a 
+    # options : (dict) Dictionary of solver options, should at least have a
 
     # The parameters for which a gradient should be computed are passed as a flat list of tensors to the forward function
     # The gradient returned by backward() will take the same shape.
-    #flat_params = flatten_grad_params(func.parameters())
+    # flat_params = flatten_grad_params(func.parameters())
     params = find_parameters(func)
 
     # Forward integrating the NODE and returning the state at each evaluation step
@@ -193,39 +189,45 @@ def flatten_grad_params(params):
 
     return torch.cat(flat_params) if len(flat_params) > 0 else torch.tensor([])
 
+
 def flatten_params(params):
     # values in the params tuple are flattened and returned as a list
     flat_params = [p.contiguous().view(-1) for p in params]
     return torch.cat(flat_params) if len(flat_params) > 0 else torch.tensor([])
 
-def get_integration_options(n0,n1,dt,substeps):
+
+def get_integration_options(n0, n1, dt, substeps):
 
     sub_dt = float(dt / substeps)
-    nSteps = substeps*(n1-n0)
-    
+    nSteps = substeps * (n1 - n0)
+
     integration_options = {
-                            't0':n0*dt,
-                            'dt':sub_dt,
-                            'nSteps':nSteps,
-                            'eval_idx':np.arange(0,nSteps+1,substeps)
-                        }
+        "t0": n0 * dt,
+        "dt": sub_dt,
+        "nSteps": nSteps,
+        "eval_idx": np.arange(0, nSteps + 1, substeps),
+    }
 
     return integration_options
+
 
 def find_parameters(module):
 
     assert isinstance(module, nn.Module)
 
     # If called within DataParallel, parameters won't appear in module.parameters().
-    if getattr(module, '_is_replica', False):
+    if getattr(module, "_is_replica", False):
 
         def find_tensor_attributes(module):
-            tuples = [(k, v) for k, v in module.__dict__.items() if torch.is_tensor(v) and v.requires_grad]
+            tuples = [
+                (k, v)
+                for k, v in module.__dict__.items()
+                if torch.is_tensor(v) and v.requires_grad
+            ]
             return tuples
 
         gen = module._named_members(get_members_fn=find_tensor_attributes)
         return [param for _, param in gen]
     else:
-        
+
         return [r for r in module.parameters() if r.requires_grad]
-        
