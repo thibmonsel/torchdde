971354e9cd72152e686ba73a8d7f731f3cf4415c
Thibault Monsel <thibaultmonsel@gmail.com>
2023-10-20 17:46:43 +0200

    removed experimental experiments

--- sshadd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sshfs margaret:/home/tau/tmonsel/torchdde_exp /home/monsel/Desktop/torchdde/margaret 
-
-sshfs titanic:/home/tau/tmonsel/torchdde_exp /home/monsel/Desktop/torchdde/titanic 
