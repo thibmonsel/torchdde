aee7555d2bc5ea3596ad0d60c50aafcb54499489
Thibault Monsel <thibaultmonsel@gmail.com>
2023-10-20 17:59:24 +0200

    added version of torchdde

--- torchdde/__init__.py
+++ torchdde/__init__.py
@@ -2,3 +2,5 @@ from .dde_adjoint import ddesolve_adjoint
 from .interpolation import TorchLinearInterpolator
 from .ode_adjoint import odesolve_adjoint
 from .solver import RK2, RK4, DDESolver, Euler, Ralston
+
+__version__ = "0.0.1"
