4bbf60d5da2994ec718e127c86afdc1ecee4d708
Thibault Monsel <thibaultmonsel@gmail.com>
2023-11-20 09:49:02 +0100

    fixed ode adjoint

--- torchdde/ode_adjoint.py
+++ torchdde/ode_adjoint.py
@@ -8,24 +8,6 @@ import torch.nn as nn
 from torchdde.solver.ode_solver import *
 
 
-#
-# Implements a version of the NeuralODE adjoint optimisation algorithm, with the Adaptive Checkpoint Adjoint method
-#
-# Original NODE : https://arxiv.org/abs/1806.07366
-# ACA version : https://arxiv.org/abs/2006.02493
-#
-# The forward integration is done through the scipy IVP solver.
-# The backpropagation is based on the timesteps chosen by the scipy solver and only requires a step function for
-# the relevant scheme to be added in the code.
-# Any explicit scheme available with the scipy solver can be easily added here.
-
-#
-# NB : This code is heavily based on the torch_ACA package (https://github.com/juntang-zhuang/torch_ACA)
-
-
-# Used for float comparisons
-
-
 class odeint_ACA(torch.autograd.Function):
     @staticmethod
     def forward(ctx, y0, func, ts, solver, *params):
@@ -42,55 +24,37 @@ class odeint_ACA(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, *grad_y):
-        # This function implements the adjoint gradient estimation method for NODEs
         # grad_output holds the gradient of the loss w.r.t. each evaluation step
         grad_output = grad_y[0]
 
-        # h is the value of the forward time step
         h = -(ctx.ts[1] - ctx.ts[0])
-        # Retrieving the time mesh and the corresponding states created in forward()
         ys = ctx.ys
         ts = ctx.ts
 
         params = ctx.saved_tensors
-        solver = ctx.solver
-        # The last step of the time mesh is an evaluation step, thus the adjoint state which corresponds to the
-        # gradient of the loss w.r.t. the evaluation states is initialised with the gradient corresponding to
-        # the last evaluation time.
-
         adjoint_state = torch.zeros_like(grad_output[:, -1])
-        # The adjoint state as well as the parameters' gradient are integrated backwards in time.
-        # Following the Adaptive Checkpoint Adjoint method, the time steps and corresponding states of the forward
-        # integration are re-used by going backwards in the time mesh.
 
         out2 = None
-        for i, t in enumerate(reversed(ts)):
-            # adjoint_state -= grad_output[:, -j - 1]
-            # Backward Integrating the adjoint state and the parameters' gradient between time i and i-1
-            y_t = torch.autograd.Variable(ys[:, i - 1], requires_grad=True)
+        for i in range(len(ts), 0, -1):
+            adjoint_state -= grad_output[:, i]
+            z_var = torch.autograd.Variable(ys[:, i - 1], requires_grad=True)
 
             with torch.enable_grad():
-                out = ctx.func(t, y_t)
-                adj_fn = lambda t, adj_y: torch.autograd.grad(
-                    out, y_t, -adj_y, retain_graph=True
-                )[0]
-                adjoint_state = solver.step(adj_fn, t, adjoint_state, h)
-
-                # Computing the increment to the parameters' gradient corresponding to the current time step
+                out = ctx.func(ts[i - 1], z_var)
                 param_inc = torch.autograd.grad(
                     out, params, -adjoint_state, retain_graph=True
                 )
+                adjoint_state = torch.autograd.grad(out, z_var, -adjoint_state)[0]
 
-                # The following line corresponds to an integration step of the adjoint state
-
-            # incrementing the parameters' grad
             if out2 is None:
-                out2 = tuple([-h * p for p in param_inc])
+                out2 = tuple([h * p for p in param_inc])
             else:
                 for _1, _2 in zip([*out2], [*param_inc]):
-                    _1 -= h * _2
+                    _1 += h * _2
 
-        return adjoint_state, None, None, None, *out2
+        out = adjoint_state, None, None, None, *out2
+
+        return out
 
 
 def odesolve_adjoint(z0, func, ts, solver):
