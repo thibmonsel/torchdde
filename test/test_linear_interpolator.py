1b1c1e709e6d2642607b6d37d293255e86c2cc3d
Thibault Monsel <thibaultmonsel@gmail.com>
2024-02-22 14:47:02 +0100

    added ruff, pyright, tests on first files

---
+++ test/test_linear_interpolator.py
@@ -0,0 +1,102 @@
+import pytest
+import torch
+from torchdde import TorchLinearInterpolator
+
+
+def test_2d_ts():
+    ts = torch.linspace(0, 10, 10).reshape(-1, 2)
+    ys = torch.ones(10, 10, 1)
+    with pytest.raises(ValueError, match=r"`ts` must be one dimensional."):
+        TorchLinearInterpolator(ts, ys)
+
+
+def test_non_montonic_ts():
+    ts = torch.linspace(0, 10, 10)
+    ts = torch.roll(ts, 1)
+    ys = torch.ones(10, 10, 1)
+    with pytest.raises(ValueError, match=r"`ts` must be monotonically increasing."):
+        TorchLinearInterpolator(ts, ys)
+
+
+def test_ts_ys_shape_mismatch():
+    ts = torch.linspace(0, 10, 10)
+    ys = torch.ones(10, 8, 1)
+    with pytest.raises(ValueError):
+        TorchLinearInterpolator(ts, ys)
+
+
+def test_call():
+    ts = torch.linspace(0, 10, 10)
+    ys = torch.ones(10, 10, 1)
+    inter = TorchLinearInterpolator(ts, ys)
+    inter(ts[2].reshape(1))
+    inter(1.2)
+
+
+def test_linear_interpolator():
+    ts = torch.linspace(1.0, 10.0, 10)
+    ys = torch.arange(1, 11, dtype=torch.float32)[None, ..., None]
+    inter = TorchLinearInterpolator(ts, ys)
+
+    assert torch.allclose(inter(1.0), ys[0, 0])
+    assert torch.allclose(inter(ts[0]), ys[0, 0])
+    assert torch.allclose(inter(8.0), ys[0, 7])
+    assert torch.allclose(inter(ts[7]), ys[0, 7])
+    assert torch.allclose(inter(7.2), torch.tensor(7.2))
+    assert torch.allclose(inter(5.4), torch.tensor(5.4))
+
+
+def test_add_point_t():
+    ts = torch.linspace(1.0, 10.0, 10)
+    ys = torch.arange(1, 11, dtype=torch.float32)[None, ..., None]
+    inter = TorchLinearInterpolator(ts, ys)
+    inter.add_point(torch.tensor(11.0), torch.tensor([[1.0]]))
+
+
+def test_add_point_t_mishape():
+    ts = torch.linspace(1.0, 10.0, 10)
+    ys = torch.arange(1, 11, dtype=torch.float32)[None, ..., None]
+    inter = TorchLinearInterpolator(ts, ys)
+    with pytest.raises(RuntimeError):
+        inter.add_point(torch.tensor([1.0, 2.0]), torch.tensor([[1.0]]))
+
+
+def test_add_point_y_mishape():
+    ts = torch.linspace(1.0, 10.0, 10)
+    ys = torch.arange(1, 11, dtype=torch.float32)[None, ..., None]
+    inter = TorchLinearInterpolator(ts, ys)
+    with pytest.raises(
+        ValueError,
+        match=r"You tried to add a new value that doesn't fit the shape of self.ys ",
+    ):
+        inter.add_point(torch.tensor(11.0), torch.tensor([[2.0, 1.0]]))
+
+
+def test_add_point_warning():
+    ts = torch.linspace(1.0, 10.0, 10)
+    ys = torch.arange(1, 11, dtype=torch.float32)[None, ..., None]
+    inter = TorchLinearInterpolator(ts, ys)
+    new_t = ts[0]
+    with pytest.warns(
+        UserWarning,
+        match=f"already have new_t={new_t} point in interpolation, overwriting it ",
+    ):
+        inter.add_point(new_t, torch.tensor([[2.0]]))
+
+
+def test_add_point_end():
+    ts = torch.linspace(1.0, 10.0, 10)
+    ys = torch.arange(1, 11, dtype=torch.float32)[None, ..., None]
+    inter = TorchLinearInterpolator(ts, ys)
+    new_t = torch.tensor(11.0)
+    inter.add_point(new_t, torch.tensor([[2.0]]))
+    assert torch.allclose(inter.ts[-1], new_t)
+
+
+def test_add_point_start():
+    ts = torch.linspace(1.0, 10.0, 10)
+    ys = torch.arange(1, 11, dtype=torch.float32)[None, ..., None]
+    inter = TorchLinearInterpolator(ts, ys)
+    new_t = torch.tensor(-1.0)
+    inter.add_point(new_t, torch.tensor([[2.0]]))
+    assert torch.allclose(inter.ts[0], new_t)
--- torchdde/interpolation/linear_interpolation.py
+++ torchdde/interpolation/linear_interpolation.py
@@ -1,40 +1,47 @@
 import warnings
+from typing import Union
 
 import torch
 from jaxtyping import Float
+
 
 tiny = 10e-3
 
 
 class TorchLinearInterpolator:
-    r"""Linear interpolator class that is compatible with batching."""
+    r"""Linear interpolator class that is compatible with batching.
+    All elements in batch must have the same ts."""
 
     def __init__(
         self,
-        ts: Float[torch.Tensor, "time"],
+        ts: Float[torch.Tensor, " time"],
         ys: Float[torch.Tensor, "batch time ..."],
-    ):
-        self.ts = ts  # [N_t]
-        self.ys = ys  # [N, N_t, D]
-
-    def __post_init__(self):
-        if self.ts.ndim != 1:
+    ) -> None:
+        if ts.ndim != 1:
             raise ValueError("`ts` must be one dimensional.")
 
-        if self.ys.shape[1] != self.ts.shape[0]:
+        if ys.shape[1] != ts.shape[0]:
             raise ValueError(
                 "Must have ts.shape[0] == ys.shape[0], that is to say the same "
                 "number of entries along the timelike dimension."
             )
 
-        if not torch.all(torch.diff(self.ts) > 0):
+        if not torch.all(torch.diff(ts) > 0):
             raise ValueError("`ts` must be monotonically increasing.")
+
+        self.ts = ts
+        self.ys = ys
 
     def to(self, device):
         self.ys = self.ys.to(device)
         self.ts = self.ts.to(device)
 
-    def _interpret_t(self, t: torch.Tensor, left: bool):
+    def _interpret_t(
+        self, t: Float[Union[torch.Tensor, float], "#1"], left: bool
+    ) -> tuple[
+        Float[torch.Tensor, ""],
+        Union[Float[torch.Tensor, "1"], float],
+    ]:
         maxlen = self.ts.shape[0] - 2
         index = torch.searchsorted(self.ts, t, side="left" if left else "right")
         index = torch.clip(index - 1, 0, maxlen)
@@ -42,7 +49,9 @@ class TorchLinearInterpolator:
         fractional_part = t - self.ts[index]
         return index, fractional_part
 
-    def __call__(self, t: Float[torch.Tensor, "1"], left=True):
+    def __call__(
+        self, t: Union[Float[torch.Tensor, ""], float], left=True
+    ) -> Float[torch.Tensor, " batch ..."]:
         if t > self.ts[-1] or t < self.ts[0]:
             if self.ys.shape[1] == 1:
                 return self.ys[:, 0]
@@ -51,11 +60,11 @@ class TorchLinearInterpolator:
             if torch.abs((t - self.ts[-1])) < tiny:
                 return self.ys[:, -1]
             raise ValueError(
-                f"Interpolation point is outside data range. ie t={t} > ts[-1]={self.ts[-1]} or t < ts[0]={self.ts[0]}"
+                "Interpolation point is outside data range. "
+                + f"ie t={t} > ts[-1]={self.ts[-1]} or t < ts[0]={self.ts[0]}"
             )
 
         index, fractional_part = self._interpret_t(t, left)
-
         prev_ys = self.ys[:, index]
         next_ys = self.ys[:, index + 1]
         prev_t = self.ts[index]
@@ -65,29 +74,27 @@ class TorchLinearInterpolator:
 
     def add_point(
         self,
-        new_t: Float[torch.Tensor, "1"],
+        new_t: Float[torch.Tensor, ""],
         new_y: Float[torch.Tensor, "batch ..."],
-    ):
+    ) -> None:
         if new_t in self.ts:
             warnings.warn(
                 f"already have new_t={new_t} point in interpolation, overwriting it "
             )
 
-        # new_y = new_y.to(self.ts.device)
         new_y = torch.unsqueeze(new_y, dim=1)
         new_t = torch.unsqueeze(new_t.clone(), dim=0)
-        # new_t = new_t.to(self.ts.device)
 
         if self.ys.shape[-1] != new_y.shape[-1]:
             raise ValueError(
                 "You tried to add a new value that doesn't fit the shape of self.ys "
             )
         rel_position = self.ts < new_t
-        last_insertion = torch.all(rel_position)
-        if last_insertion:
+        last_insertion = torch.sum(rel_position)
+        if last_insertion == len(rel_position):
             new_ys = torch.concat((self.ys, new_y), dim=1)
             new_ts = torch.concat((self.ts, new_t))
-        elif not last_insertion:
+        elif last_insertion == 0:
             new_ys = torch.concat((new_y, self.ys), dim=1)
             new_ts = torch.concat((new_t, self.ts))
         else:
